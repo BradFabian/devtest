a282a170fec79854b41685afd3f8a2a10af216c5
Brad <bradleofabian@gmail.com>
2020-06-26 22:04:39 -0400

    finsihed test of getting headlines

--- js/app.js
+++ js/app.js
@@ -1,23 +1,21 @@
-fetchHeadlines = () => {
-  fetch("https://devtest.equisolve-dev.com/")
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      data.sort((a, b) => {
-        let da = new Date(a.published_at),
-          db = new Date(b.published_at);
-        return db - da;
-      });
-
-      data.forEach((headlines) => {
-        let li = document.createElement("li");
-        li.classList.add("title");
-        li.innerText = `${headlines.published_at}: ${headlines.title}`;
-        document.querySelector(".headlines").appendChild(li);
-      });
-    })
-    .catch(function (err) {
-      console.log(err);
+fetch("https://devtest.equisolve-dev.com/")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    data.sort((a, b) => {
+      let da = new Date(a.published_at),
+        db = new Date(b.published_at);
+      return db - da;
     });
-};
+
+    data.forEach((headlines) => {
+      let li = document.createElement("li");
+      li.classList.add("headlines__title");
+      li.innerText = `${headlines.published_at}- ${headlines.title}`;
+      document.querySelector(".headlines").appendChild(li);
+    });
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
